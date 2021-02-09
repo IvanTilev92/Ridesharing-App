f8c4c3286df553a162c2897cfa9bb23972558721
Ivan Tilev <iivantilev@gmail.com>
2021-02-09 20:26:47 +0100

    Updating the the Usr and Rider Annotation models.

--- Ridesharing App/UserAnnotation.swift
+++ Ridesharing App/UserAnnotation.swift
@@ -12,9 +12,10 @@ import MapKit
 class UserAnnotation: NSObject, MKAnnotation {
     
     var title: String?
+    var subtitle: String?
     var coordinate: CLLocationCoordinate2D
     
-    init(title: String, coordinate: CLLocationCoordinate2D) {
+    init(title: String ,subtitle: String, coordinate: CLLocationCoordinate2D) {
         self.title = title
         self.coordinate = coordinate
     }
